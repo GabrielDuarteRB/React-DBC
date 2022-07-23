d291678c7eb7faf17e24bbaf6194ad478bf202c0
Luis Carlos <crluis521@gmail.com>
2022-07-23 18:07:19 -0300

    imagem carregada no subpost

--- projetoFinal/nytimes/src/pages/PagePost/PagePost.js
+++ projetoFinal/nytimes/src/pages/PagePost/PagePost.js
@@ -9,29 +9,42 @@ const PagePost = () => {
     url = url.split('/') 
 
     const [resultado, setResultado] = useState([])
-    
+    const [urlImg, setUrlImg] = useState('')
 
     const api = async () => {
         try {
-          const {data} = await axios.get(`https://api.nytimes.com/svc/topstories/v2/${url[3]}.json?api-key=3PqlwJMMQnNMyrgjuqPAQsdarujrJSkA`)
-          setResultado(data.results)        
+          const {data} = await axios.get(`https://api.nytimes.com/svc/topstories/v2/${url[3]}.json?api-key=wRefsADGPRpDZ6hLhaEVEVtTAmUXEOyW`)
+          console.log(data.results)
+          data.results.map((item, i) =>{
+            if(parseInt(i) === parseInt(url[2])) {
+                item.multimedia.forEach((x) =>{
+                  setUrlImg(x.url)
+                })
+                console.log('teste')
+                setResultado(item)
+            }
+          })     
         } catch (error) {
           console.log('caiu no erro', error)
         }
     }
-    
     useEffect(() => {
         api()
     }, [])
 
-
+    console.log(`esse eh o resultado: `, resultado)   
   return (
     <div>
-        <HeaderPagePost 
-        facet={[resultado.des_facet]}
-        />
-        <PostApi 
-            api={resultado}
+        {/* <HeaderPagePost 
+        facet={[resultado.title]}
+        /> */}
+        <Post
+        titulo={resultado.title}
+        conteudo={resultado.abstract}
+        urlImagem={urlImg}
+        altura='100px'
+        largura='120px'
+
         />
     </div>
   )
